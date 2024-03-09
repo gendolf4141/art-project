ddd31162309336052b592240d113d5c9b070b9a9
gennady.urusov <gennady.urusov@simbirsoft.com>
2024-03-09 08:08:10 +0300

    V-3: Add requirements.txt
    V-3: Add requirements.txt

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Устанавливаем необходимые зависимости (если еще не установлены)
-pip install pyinstaller
+pip install virtualenv
+python -m venv venv
+venv/Scripts/activate
+pip install -r requirements.txt
 
 # Переходим в директорию проекта
 #cd /path/to/customtkinter
 
 # Собираем проект с помощью PyInstaller
-pyinstaller --onefile --noconsole main.py
-
+#pyinstaller --onefile --noconsole main.py
+#pyinstaller --noconfirm --onedir --windowed --add-data "<CustomTkinter Location>/customtkinter;customtkinter/"  "main.py"
+pyinstaller -F --onefile --noconsole main.py --collect-all customtkinter
 # Перемещаем .exe файл из директории dist в желаемую директорию
-mv dist/main.exe ./art-project.exe
+#mv dist/main.exe ./art-project.exe
 
 # Очищаем временные файлы, созданные PyInstaller
 #rm -rf build dist __pycache__ *.spec
